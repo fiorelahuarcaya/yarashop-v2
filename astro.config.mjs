4d7b748055be7975356306be4041f63c2a1acbb6
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-09 12:38:08 -0500

    bridge

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), react()]
 });
