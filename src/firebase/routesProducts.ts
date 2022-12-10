0fe294d3f3630cea18cacae47c74b85dbdb680c7
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-12-09 20:55:02 -0500

    format

--- src/firebase/routesProducts.ts
+++ src/firebase/routesProducts.ts
@@ -1,29 +1,34 @@
-import { onSnapshot, query, orderBy, collection, addDoc, doc, updateDoc, deleteDoc, getDoc, where} from "firebase/firestore";
+import {
+  onSnapshot,
+  query,
+  orderBy,
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+  getDoc,
+  where,
+} from "firebase/firestore";
 import type { QuerySnapshot, DocumentData } from "firebase/firestore";
-import {db} from "./db.js"
+import { db } from "./db.js";
 import type { Producto } from "../types/Producto.js";
-
 
 export async function getProducts(
   cb: (doc: QuerySnapshot<DocumentData>) => void
-){
+) {
   const products = collection(db, "Productos");
-  const q = query(
-    products,
-    orderBy("nombre", "asc")
-  );
+  const q = query(products, orderBy("nombre", "asc"));
   const unsub = onSnapshot(q, (coll) => cb(coll));
   return unsub;
 }
 
 // Read a collection from "Productos" ID
-export async function getProductByID(productId: string
-){
+export async function getProductByID(productId: string) {
   const products = doc(db, "Productos", productId);
   const docSnap = await getDoc(products);
   return docSnap;
 }
-
 
 // Add a new document in collection "Productos"
 export async function createProduct(product: Producto) {
