7df55c7b56648e8ff54140c29781f5461be90f3a
Fiorela Huarcaya <65567785+fiorelahuarcaya@users.noreply.github.com>
2022-12-09 21:57:14 -0500

    Ejemplo CI/CD (#2)
    * format
    
    * home

--- src/firebase/routesTienda.ts
+++ src/firebase/routesTienda.ts
@@ -1,25 +1,36 @@
-import {onSnapshot, doc,query, orderBy, collection, getDoc, addDoc, updateDoc, getDocs, where, writeBatch} from "firebase/firestore";
-import type { QuerySnapshot, DocumentData  } from "firebase/firestore";
+import {
+  onSnapshot,
+  doc,
+  query,
+  orderBy,
+  collection,
+  getDoc,
+  addDoc,
+  updateDoc,
+  getDocs,
+  where,
+  writeBatch,
+} from "firebase/firestore";
+import type { QuerySnapshot, DocumentData } from "firebase/firestore";
 
-
-import {db} from "./db"
+import { db } from "./db";
 import type { ProductoTienda, Venta } from "../types/Tienda";
 
-export async function getTiendas(idTienda : string){
-    const tiendas = doc(db, "Tienda", idTienda);
-    const docSnap = await getDoc(tiendas);
+export async function getTiendas(idTienda: string) {
+  const tiendas = doc(db, "Tienda", idTienda);
+  const docSnap = await getDoc(tiendas);
 
-    return docSnap;
-  }
+  return docSnap;
+}
 
-  export async function getProductoByTienda(idTienda : string){
-    const productsCollection = collection(db, `Tienda/${idTienda}/Producto`);
+export async function getProductoByTienda(idTienda: string) {
+  const productsCollection = collection(db, `Tienda/${idTienda}/Producto`);
 
-    const q = query(productsCollection)
-    const docsSnap = await getDocs(q)
+  const q = query(productsCollection);
+  const docsSnap = await getDocs(q);
 
-    return docsSnap;
-  }
+  return docsSnap;
+}
 
 // export async function createVenta(idTienda : string, venta: Venta) {
 //     const ventaData: Venta = {
@@ -29,56 +40,59 @@ export async function getTiendas(idTienda : string){
 //     return await addDoc(collection(db, `Tienda/${idTienda}/Venta`), ventaData);
 //   }
 
+export async function createVenta(idTienda: string, ventaData: Venta) {
+  // Get a new write batch
+  // const batch = writeBatch(db);
+  // Añadir un documento a la colección VENTA
+  //
+  // Set the "Venta"
+  await addDoc(collection(db, `Tienda/${idTienda}/Venta`), ventaData);
+  // // Get a new write batch
+  const batch = writeBatch(db);
 
-    export async function createVenta(idTienda : string, ventaData: Venta) {
-    // Get a new write batch
-    // const batch = writeBatch(db);
-    // Añadir un documento a la colección VENTA  
-    //
-    // Set the "Venta"
-    await addDoc(collection(db, `Tienda/${idTienda}/Venta`), ventaData);
-// // Get a new write batch
-    const batch = writeBatch(db);
+  //     // Set the value of 'NYC'
+  //     const productsCollection = collection(db, `Tienda/${idTienda}/Venta`);
+  //     const q = query(productsCollection)
+  //     const docsSnap = await getDocs(q)
 
-//     // Set the value of 'NYC'
-//     const productsCollection = collection(db, `Tienda/${idTienda}/Venta`);
-//     const q = query(productsCollection)
-//     const docsSnap = await getDocs(q)
+  //     const tiendaRef = doc(db, "Tienda", idTienda);
 
-//     const tiendaRef = doc(db, "Tienda", idTienda);
+  //     batch.set(tiendaRef, ventaData);
 
-//     batch.set(tiendaRef, ventaData);
+  //     // // Update the population of 'SF'
+  //     // const sfRef = doc(db, "cities", "SF");
+  //     // batch.update(sfRef, {"population": 1000000});
 
-//     // // Update the population of 'SF'
-//     // const sfRef = doc(db, "cities", "SF");
-//     // batch.update(sfRef, {"population": 1000000});
+  //     // // Delete the city 'LA'
+  //     // const laRef = doc(db, "cities", "LA");
+  //     // batch.delete(laRef);
 
-//     // // Delete the city 'LA'
-//     // const laRef = doc(db, "cities", "LA");
-//     // batch.delete(laRef);
+  //     // Commit the batch
+  //     await batch.commit();
 
-//     // Commit the batch
-//     await batch.commit();
-    
+  // Actualizar los documentos de la colección PRODUCTO dentro de la colección TIENDA, cuyos IDs aparezcan en la lista del objeto "Venta"
+  ventaData.lista.forEach(async (item) => {
+    console.log(item);
 
-    // Actualizar los documentos de la colección PRODUCTO dentro de la colección TIENDA, cuyos IDs aparezcan en la lista del objeto "Venta"
-    ventaData.lista.forEach(async (item)=>{
-        console.log(item);
+    const q = query(
+      collection(db, `Tienda/${idTienda}/Producto`),
+      where("idProducto", "==", item.idProducto)
+    );
+    const querySnapshot = getDocs(q);
 
-        const q = query(collection(db, `Tienda/${idTienda}/Producto`), where("idProducto", "==", item.idProducto));
-        const querySnapshot = getDocs(q);
+    let docProducto = (await querySnapshot).docs[0];
+    console.log(docProducto.data);
+    // batch.update(querySnapshot[0], { "cantidad": - item.cantidad });
 
-        let docProducto= (await querySnapshot).docs[0];
-        console.log(docProducto.data)
-        // batch.update(querySnapshot[0], { "cantidad": - item.cantidad });
+    const productRef = doc(db, `Tienda/${idTienda}/Producto`, docProducto.id);
+    console.log(productRef);
 
-        const productRef = doc(db, `Tienda/${idTienda}/Producto`, docProducto.id);
-        console.log(productRef);
+    await updateDoc(productRef, {
+      cantidad: docProducto.data().cantidad - item.cantidad,
+    });
+  });
 
-        await updateDoc(productRef, {cantidad: docProducto.data().cantidad-item.cantidad});
-    })
-
-    // ...
-    // commit the batch
-    await batch.commit();
-  }
+  // ...
+  // commit the batch
+  await batch.commit();
+}
--- src/types/Tienda.ts
+++ src/types/Tienda.ts
@@ -1,35 +1,31 @@
+export type ProductoTienda = {
+  idProducto: string;
+  nombre: string;
+  descripcion: string;
+  idSubcategoria: string;
+  precioCosto: number;
+  precioVentaR: number;
+  rucProvee: string;
+  cantidad: number;
+};
 
-    export type ProductoTienda = {
-    idProducto: string;
-    nombre: string;
-    descripcion: string;
-    idSubcategoria : string;
-    precioCosto: number;
-    precioVentaR: number;
-    rucProvee : string;
-    cantidad: number;
-  };
+export type ItemLista = {
+  cantidad: number;
+  idProducto: string;
+  total: number;
+};
 
-  export type ItemLista = {
-    cantidad: number;
-    idProducto: string;
-    total: number;
-  };
-  
-  export type Venta = {
-    fechaHora: string;
-    lista: ItemLista[];
-  };
+export type Venta = {
+  fechaHora: string;
+  lista: ItemLista[];
+};
 
-  export type Tienda = {
-    idTienda: string;
-    idBodeguero: string;
-    nombreTienda: string;
-    ubicacion: string;
+export type Tienda = {
+  idTienda: string;
+  idBodeguero: string;
+  nombreTienda: string;
+  ubicacion: string;
 
-    Producto : ProductoTienda[];
-    Venta : Venta[];
-  }
-
-
-  
+  Producto: ProductoTienda[];
+  Venta: Venta[];
+};
